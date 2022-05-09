8e9483827255e9c4de4e734ece82aeda10070ccd
Mayur <flynryder427@gmail.com>
2022-05-09 21:50:51 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,3 +1,8 @@
+git config --global user.email "flynryder427@gmail.com"
+git config --global user.name "xenxynon"
+git config --global color.ui false
+
+
 repo init -u $rom $extra_vars
 git clone git clone https://github.com/XenStuff/manifest --depth=1 -b $manifest_branch .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
