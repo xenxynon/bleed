a4c89318a0915b893b3c3ce3dadcb8a046cf038a
Mayur <flynryder427@gmail.com>
2022-05-09 21:46:38 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 repo init -u $rom $extra_vars
-git clone git clone https://github.com/XenStuff/manifest --depth=1 -b 404 .repo/local_manifests
+git clone git clone https://github.com/XenStuff/manifest --depth=1 -b $manifest_branch .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
