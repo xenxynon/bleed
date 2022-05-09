ed5294dd7a617650f2c57a27259d5321893c7874
Mayur <flynryder427@gmail.com>
2022-05-09 22:24:44 +0530

    And moar fuckup

--- sync.sh
+++ sync.sh
@@ -3,6 +3,6 @@ git config --global user.name "xenxynon"
 git config --global color.ui false
 
 
-repo init -u https://github.com/P-404/android_manifest --depth=1 --no-repo-verify -g default,-mips,-darwin,-notdefault
-git clone git clone https://github.com/XenStuff/manifest --depth=1 -b 404 .repo/local_manifests
+repo init -u https://github.com/P-404/android_manifest --depth=1
+git clone https://github.com/XenStuff/manifest --depth=1 -b 404 .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
