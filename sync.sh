e2b4ae83db4237a2e302b8f35c5bd3b8e53bccc4
Mayur <flynryder427@gmail.com>
2022-05-09 22:14:21 +0530

    sync properly [skip ci]

--- sync.sh
+++ sync.sh
@@ -3,6 +3,6 @@ git config --global user.name "xenxynon"
 git config --global color.ui false
 
 
-repo init -u $rom $extra_vars
+repo init -u https://github.com/P-404/android_manifest --depth=1 --no-repo-verify -g default,-mips,-darwin,-notdefault
 git clone git clone https://github.com/XenStuff/manifest --depth=1 -b $manifest_branch .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
