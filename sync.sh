8713216dba5d7d24bbc927714cbbcf2de5998d92
Mayur <flynryder427@gmail.com>
2022-05-09 22:40:20 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -2,7 +2,9 @@ git config --global user.email "flynryder427@gmail.com"
 git config --global user.name "xenxynon"
 git config --global color.ui false
 
-
+# sync
 repo init -u https://github.com/P-404/android_manifest --depth=1
 git clone https://github.com/XenStuff/manifest --depth=1 -b 404 .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
+# 1
