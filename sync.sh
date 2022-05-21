2c8cb483998af62e5be08421db6f425700104f21
Mayur <flynryder427@gmail.com>
2022-05-21 07:22:09 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -5,6 +5,6 @@ git config --global color.ui false
 # sync
 repo init -u https://github.com/P-404/android_manifest --depth=1
 git clone https://github.com/xenxynon-lab-404/manifest --depth=1 -b 404 .repo/local_manifests
-repo sync -c -j(nproc --all) --force-sync --no-clone-bundle --no-tags -v
+repo sync -c --force-sync --no-clone-bundle --no-tags -v
 
 # ban bsdk
