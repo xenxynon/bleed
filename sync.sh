41ef0246716e36f749d540d5e044ecc034d98879
nohaxrobot <97295772+nohaxrobot@users.noreply.github.com>
2022-05-25 14:38:46 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 git config --global user.email "flynryder427@gmail.com"
 git config --global user.name "xenxynon"
 git config --global color.ui false
-
+# sus
 # sync
 repo init -u https://github.com/P-404/android_manifest --depth=1
 git clone https://github.com/xenxynon-lab-404/manifest --depth=1 -b 404-mi439 .repo/local_manifests
