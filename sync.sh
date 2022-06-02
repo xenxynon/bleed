7599beede485d8cd73d15be78272b401f07372e1
Mayur <flynryder427@gmail.com>
2022-06-02 09:35:45 +0530

    Build it plox

--- sync.sh
+++ sync.sh
@@ -6,5 +6,4 @@ git config --global color.ui false
 repo init -u https://github.com/ProtonPlus-org/manifest -b sc-v2 --depth=1
 git clone https://github.com/XenStuff/manifest --depth=1 -b proton .repo/local_manifests
 repo sync -c --force-sync -j8 --no-clone-bundle --no-tags
-
 # build????
