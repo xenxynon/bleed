fec47308ad2db71b493c81dd8a676cfff7d2b6ea
Mayur <flynryder427@gmail.com>
2022-06-02 17:47:26 +0530

    This final

--- sync.sh
+++ sync.sh
@@ -2,6 +2,7 @@ git config --global user.email "flynryder427@gmail.com"
 git config --global user.name "xenxynon"
 git config --global color.ui false
 
+
 # sync
 repo init -u https://github.com/ProtonPlus-org/manifest -b sc-v2 --depth=1
 git clone https://github.com/XenStuff/manifest --depth=1 -b proton .repo/local_manifests
