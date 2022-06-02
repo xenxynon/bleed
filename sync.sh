1171a06549e60d4be0958a9972c274bf10173eb6
Mayur <flynryder427@gmail.com>
2022-06-02 06:45:37 +0530

    [skip ci]

--- sync.sh
+++ sync.sh
@@ -3,8 +3,8 @@ git config --global user.name "xenxynon"
 git config --global color.ui false
 
 # sync
-repo init -u https://github.com/P-404/android_manifest --depth=1
-git clone https://github.com/xenxynon-lab-404/manifest --depth=1 -b 404-mi439 .repo/local_manifests
+repo init -u https://github.com/ProtonPlus-org/manifest -b sc-v2 --depth=1
+git clone https://github.com/XenStuff/manifest --depth=1 -b proton .repo/local_manifests
 repo sync -c --force-sync -j16 --no-clone-bundle --no-tags
 
 # build????
