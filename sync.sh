613e0609069757c99fe4bf4aab2a7f756ef4bd94
Mayur <flynryder427@gmail.com>
2022-06-02 07:53:35 +0530

    Sync it

--- sync.sh
+++ sync.sh
@@ -5,6 +5,6 @@ git config --global color.ui false
 # sync
 repo init -u https://github.com/ProtonPlus-org/manifest -b sc-v2 --depth=1
 git clone https://github.com/XenStuff/manifest --depth=1 -b proton .repo/local_manifests
-repo sync -c --force-sync -j16 --no-clone-bundle --no-tags
+repo sync -c --force-sync -j8 --no-clone-bundle --no-tags
 
 # build????
