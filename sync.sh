81ce314cf7d0c54049a1651f3c8a5ea5c9d724d5
Mayur <flynryder427@gmail.com>
2022-06-09 09:55:12 +0530

    404 time again

--- sync.sh
+++ sync.sh
@@ -4,9 +4,11 @@ git config --global color.ui false
 
 
 # sync
-repo init -u https://github.com/ProtonPlus-org/manifest -b sc-v2 --depth=1
-git clone https://github.com/XenStuff/manifest --depth=1 -b proton .repo/local_manifests
+repo init -u https://github.com/P-404/android_manifest --depth=1
+git clone https://github.com/xenxynon-lab-404/manifest --depth=1 -b 404-mi439 .repo/local_manifests
 repo sync -c --force-sync -j8 --no-clone-bundle --no-tags
-rm -rf packages/resources/devicesettings
-git clone https://github.com/lineageos/android\_packages\_resources\_devicesettings packages/resources/devicesettings
+
 # build????
+source build/envsetup.sh
+lunch p404_Mi439-eng
+make bacon
