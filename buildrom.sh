de7c448c56b4d28d658e53a9588bfd5a2c9b027c
Mayur <flynryder427@gmail.com>
2022-06-09 11:54:05 +0530

    [skip ci]

--- buildrom.sh
+++ buildrom.sh
@@ -7,8 +7,3 @@ git config --global color.ui false
 repo init -u https://github.com/P-404/android_manifest --depth=1
 git clone https://github.com/xenxynon-lab-404/manifest --depth=1 -b 404-mi439 .repo/local_manifests
 repo sync -c --force-sync -j8 --no-clone-bundle --no-tags
-
-# build????
-source build/envsetup.sh
-lunch p404_Mi439-eng
-make bacon
